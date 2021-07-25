d379953b1e8613d3cde7527b3877011a47f8afe1
AhmeddNasr <ahmeddhemd@gmail.com>
2021-07-26 01:45:57 +0200

    changed the way flipping a card work
    new cards will always start with the questions being on the front regardless of previous card flip status

--- src/studyHelper/Card.js
+++ src/studyHelper/Card.js
@@ -1,18 +1,33 @@
-import { useState } from 'react';
 function Card(props) {
-
     let cardData = props.flashcard;
-    const [isAnswerActive, setActive] = useState(false);
+    // const [isAnswerActive, setActive] = useState(false);
+    // const handleClick = () => {
+    //     setActive(!isAnswerActive);
+    // }
+
     const handleClick = () => {
-        setActive(!isAnswerActive);
+        let title = document.querySelector('.flashcard-title');
+        let definition = document.querySelector('.flashcard-definition');
+        if (title.classList.value === 'flashcard-title active') {
+            title.classList.value = 'flashcard-title hidden';
+            definition.classList.value = 'flashcard-definition active';
+            // title.classList.remove('active');
+            // title.classList.add('hidden');
+            // definition.classList.add('active');
+            // definition.classList.remove('hidden');    
+        } else {
+            title.classList.value = 'flashcard-title active';
+            definition.classList.value = 'flashcard-definition hidden';
+        }
     }
+
     return(
         <div className='flashcard-inner' key={cardData.termID + '/' + cardData.questionID} onClick={() => handleClick()}>
-            <div className={'flashcard-title ' + (isAnswerActive ? 'active' : 'hidden')}>
+            <div className='flashcard-title active'>
                 <p>{cardData.title}</p>
                 <p>{cardData.question}</p>
             </div>
-            <div className={'flashcard-definition ' + (isAnswerActive ? 'hidden' : 'active')}>
+            <div className='flashcard-definition hidden'>
                 <p>{cardData.answer}</p>
             </div>
         </div>

50ff4e18aee56aaf1c09abf2858bd666b9de914c
AhmeddNasr <ahmeddhemd@gmail.com>
2021-08-05 11:25:05 +0200

    Redirect to login page if user is not logged in
    redirect to google login page if user tries to access any react route

--- src/index.js
+++ src/index.js
@@ -5,15 +5,56 @@ import App from './App';
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import Index from './studyHelper/Index';
 import Folder from './studyHelper/Folder';
+
+function HandleAuthentication() {
+  const [authenticationHandled, setAuthenticationHandled] = React.useState([false, false]);
+
+  const updateAuthentication = (bool) => {
+    console.log(bool);
+    setAuthenticationHandled([true, bool]);
+  };
+
+  function isLoggedIn() {
+    fetch('http://localhost:8080/loggedin', {credentials: 'include'})
+    .then((response => response.json()))
+    .then((data) => updateAuthentication(data.logged_in))
+    .catch((err) => console.log(err));
+  }
+
+  React.useEffect(() => {
+    isLoggedIn();
+  }, []);
+  
+  if(authenticationHandled[0]) {
+    if(!authenticationHandled[1]) {
+      window.location.href = "http://localhost:8080/google";
+      return(null);
+    }
+  }
+
+  if(!authenticationHandled[0]) {
+    return(null);
+  } else {
+    if (authenticationHandled[1]) {
+      return (
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={App} />
+            <Route exact path="/study" component={Index}></Route>
+            <Route path="/study/folder/:folderID" component={Folder} />
+          </Switch>
+        </BrowserRouter>
+      )
+    } else {
+      window.location.href = "http://www.example.com"
+    }
+  }
+}
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route exact path="/study" component={Index}/>
-        <Route path="/study/folder/:folderID" component={Folder} />
-      </Switch>
-    </BrowserRouter>
+    <HandleAuthentication />
   </React.StrictMode>,
   document.getElementById('root')
 );

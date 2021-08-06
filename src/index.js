889a743b3ed37522d165629ea1fde7980fdee7e6
AhmeddNasr <ahmeddhemd@gmail.com>
2021-08-06 10:58:14 +0200

    redirect to google login screen if logged out
    fetch login status from backend server and redirect to google login screen if the user isn't logged out

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter, Switch, Route } from "react-router-dom"
-import Index from './studyHelper/Index';
-import Folder from './studyHelper/Folder';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Index from "./studyHelper/Index";
+import Folder from "./studyHelper/Folder";
+import CreateClass from "./studyHelper/CreateClass";
+import Container from "@material-ui/core/Container";
 
 function HandleAuthentication() {
-  const [authenticationHandled, setAuthenticationHandled] = React.useState([false, false]);
+  const [authenticationHandled, setAuthenticationHandled] = React.useState([
+    false,
+    false,
+  ]);
 
   const updateAuthentication = (bool) => {
     console.log(bool);
@@ -15,25 +20,26 @@ function HandleAuthentication() {
   };
 
   function isLoggedIn() {
-    fetch('http://localhost:8080/loggedin', {credentials: 'include'})
-    .then((response => response.json()))
-    .then((data) => updateAuthentication(data.logged_in))
-    .catch((err) => console.log(err));
+    fetch("http://localhost:8080/loggedin", { credentials: "include" })
+      .then((response) => response.json())
+      .then((data) => updateAuthentication(data.logged_in))
+      .catch((err) => console.log(err));
   }
 
   React.useEffect(() => {
     isLoggedIn();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  
-  if(authenticationHandled[0]) {
-    if(!authenticationHandled[1]) {
+
+  if (authenticationHandled[0]) {
+    if (!authenticationHandled[1]) {
       window.location.href = "http://localhost:8080/google";
-      return(null);
+      return null;
     }
   }
 
-  if(!authenticationHandled[0]) {
-    return(null);
+  if (!authenticationHandled[0]) {
+    return null;
   } else {
     if (authenticationHandled[1]) {
       return (
@@ -41,22 +47,30 @@ function HandleAuthentication() {
           <Switch>
             <Route exact path="/" component={App} />
             <Route exact path="/study" component={Index}></Route>
+            <Route
+              exact
+              path="/study/create-new-class"
+              component={CreateClass}
+            ></Route>
             <Route path="/study/folder/:folderID" component={Folder} />
           </Switch>
         </BrowserRouter>
-      )
-    } else {
-      window.location.href = "http://www.example.com"
+      );
     }
   }
 }
 
+// function goToLogin() {
+//   return (
+//     window.location.assign('http://localhost:8080/google')
+//   )
+// }
 
 ReactDOM.render(
   <React.StrictMode>
     <HandleAuthentication />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function

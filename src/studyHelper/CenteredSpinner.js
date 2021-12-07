fd50540d7039b2bf0977555027e6b887b659fe67
Ahmed Nasr <ahmeddhemd@gmail.com>
2021-12-07 05:44:10 +0200

    fixed centered spinner loading not being centered
    Added a body heigh of 100% and calculate height function for centered spinner so it is actually centered with cross browser support
    
    added optional message parameter (to be ironed out later) to display a loading text (probably a hint) while fetching is happening on any route

--- src/studyHelper/CenteredSpinner.js
+++ src/studyHelper/CenteredSpinner.js
@@ -1,20 +1,39 @@
 import CircularProgress from "@material-ui/core/CircularProgress";
+import { Typography } from "@material-ui/core";
+import { useEffect } from "react";
 
-function CenteredSpinner() {
+function CenteredSpinner(props) {
+  //center the spinner
+  useEffect(() => {
+    let centeredSpinnerElement = document.getElementById(
+      "centered-spinner-container"
+    );
+    const positionFromTop = centeredSpinnerElement.getBoundingClientRect().top;
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+    centeredSpinnerElement.style.height = `${vh - positionFromTop - 40}px`;
+  }, []);
+
   return (
     <div
+      id="centered-spinner-container"
       style={{
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        direction: "row",
         width: "100%",
-        height: "100%",
       }}
     >
+      {(() => {
+        if (props.message) {
+          return <Typography variant="h5" style={{marginRight: "20px"}}>{props.message}</Typography>;
+        }
+      })()}
+
       <CircularProgress
         color="white"
         size={50}
-        style={{ height: "30%", marginTop: "30vh" }}
+        // style={{ marginTop: "30vh" }}
       />
     </div>
   );

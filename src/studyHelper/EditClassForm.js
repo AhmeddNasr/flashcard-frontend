a8333ce5fff1f53fc96d045d967791f57ce4ebfb
Ahmed Nasr <ahmeddhemd@gmail.com>
2021-12-21 21:45:19 +0200

    changed bottom pre html tag for debugging
    bottom pre changed to display formatted value that will be sent to server instead of formik values

--- src/studyHelper/EditClassForm.js
+++ src/studyHelper/EditClassForm.js
@@ -1,41 +1,52 @@
-import { Button, Grid, TextField, Typography } from "@material-ui/core";
+import {
+  Button,
+  Grid,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 import { useFormik, FormikProvider, FieldArray, FastField } from "formik";
-import React from "react";
+import React, { useRef } from "react";
 import EditTerm from "./EditTerm";
+import { Delete, InsertPhoto } from "@material-ui/icons";
 
-// function formatValues(values) {
-//   let termArray = [];
-//   let questionArray = [];
-//   // for (let i = 0; i < values.terms.length; i++) {
-//   //   if (values.terms[i].term !== "") {
-//   //     termArray.push(values.terms[i].term);
-//   //     questionArray.push(values.terms[i].questions);
-//   //     answersArray.push(values.terms[i].)
-//   //   }
-//   // }
-//   for (let i = 0; i < values.terms.length; i++) {
-//     let currentTerm = values.terms[i];
-//     console.log(currentTerm);
-//     termArray.push(currentTerm.termTitle.trim());
-//     for (let y = 0; y < currentTerm.questions.length; y++) {
-//       questionArray.push(currentTerm.questions[y]);
-//     }
-//   }
+function formatValues(values) {
+  let termArray = [];
+  let questionArray = [];
+  // for (let i = 0; i < values.terms.length; i++) {
+  //   if (values.terms[i].term !== "") {
+  //     termArray.push(values.terms[i].term);
+  //     questionArray.push(values.terms[i].questions);
+  //     answersArray.push(values.terms[i].)
+  //   }
+  // }
+  for (let i = 0; i < values.terms.length; i++) {
+    let currentTerm = values.terms[i];
+    termArray.push(currentTerm.termTitle.trim());
+    for (let y = 0; y < currentTerm.questions.length; y++) {
+      //TODO
+      questionArray.push(currentTerm.questions[y]);
+    }
+  }
 
-//   return {
-//     class_name: values.folderName.trim(),
-//     class_description: values.folderDescription.trim(),
-//     terms: termArray,
-//     questions: questionArray,
-//   };
-// }
+  return {
+    class_name: values.folderName.trim(),
+    class_description: values.folderDescription.trim(),
+    terms: termArray,
+    questions: questionArray,
+  };
+}
 
 function handleSubmit(values) {
   fetch("http://localhost:8080/api/terms/1", {
-    credentials: "include",
     method: "POST",
-    headers: { "Content-type": "application/json", Accept: "application/json" },
-    body: JSON.stringify(values),
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+      authorization: localStorage.getItem("token"),
+    },
+    body: JSON.stringify(formatValues(values)),
   })
     //TODO
     .then((data) => console.log(data))
@@ -62,8 +73,9 @@ function EditClassForm(props) {
 
   function addEmptyTerm(arrayHelpers) {
     arrayHelpers.push(emptyTerm);
-    console.log(formik.values);
   }
+
+  const imageUpload = useRef();
 
   return (
     <FormikProvider value={formik}>
@@ -141,7 +153,7 @@ function EditClassForm(props) {
             </Button>
           </Grid>
         </Grid>
-        <pre>{JSON.stringify(formik.values, null, 2, 0)}</pre>
+        <pre>{JSON.stringify(formatValues(formik.values), null, 2, 0)}</pre>
       </form>
     </FormikProvider>
   );
